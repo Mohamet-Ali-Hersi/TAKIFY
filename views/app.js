48ee6cf74503a16eb9e2240db99278a7a315f768
Eng.Mohamed <mohaalimohaali35@gmail.com>
2024-11-03 23:48:04 -0800

    Added simplisted req, res /app.js

--- views/app.js
+++ views/app.js
@@ -4,7 +4,15 @@ const HOSTNAME = 'localhost';
 const PORT = 9000;
 
 const server = http.createServer((req, res) => {
-    
+    if(req.url.startsWith('/tasks')){
+        tasksRoutes(res,res)
+    }else {
+        res.writeHead(404, 'Not Found',{ 'content-tpye': 'application/json'})
+        res.end(JSON.stringify){
+            message: 'sorry, you got lost!'
+        }
+    }
+
 });
 
 server.listen(PORT,HOSTNAME,()=>{
