2220a0d4e264c5eba985aab8aa0fb1286e887454
Eng.Mohamed <mohaalimohaali35@gmail.com>
2024-11-04 01:09:27 -0800

    Added Methods GET,POST,PATCH,DELETE  /taskRoutes.js file

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,3 +1,20 @@
 const taskRoutes=(req, res) =>{
-    
+    if(req.method === 'GET'){
+        getTasks(req, res);
+    }else if(req.method === 'POST'){
+        createTasks(req, res);
+    }
+    else if(req.method === 'PATCH'){
+        updateTasks(req, res);
+    }
+    else if(req.method === 'DELETE'){
+        deleteTasks(req, res);
+    }
+    else{
+        res.writeHead(404,'Data Not Found', {'content-type' : 'application/json'})
+        res.end(JSON.stringify({
+            message: "Unknown Method required."
+        }))
+    }
+
 }
