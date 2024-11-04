ba9fb646ee79c3249434dc133ac4b047dd15ee6c
Eng.Mohamed <mohaalimohaali35@gmail.com>
2024-11-04 10:44:47 -0800

    Added functions writeTaskFile ,readTasksFromFile /fileHandler.js file

--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -1,2 +1,18 @@
 const fs = require('fs');
 const path =require('path');
+
+const filepath = './data/tasks.json';
+
+
+const writeTaskToFile = (tasks)=>{
+    fs.writeFileSync(filepath.JSON.stringify(tasks))
+}
+
+const readTasksFromFile = ()=>{
+    if(fs.existsSync(filepath)){
+        writeTaskToFile([])
+    }
+
+    const data = fs.readFileSync(filepath);
+    return JSON.parse(data);
+}
